8b7034d2545fdade29e4be219870130a1e2cdd0c
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-01-10 19:05:31 +0900

    gradle 초기 환경 설정 저장

---
+++ TobySpringFramework/src/main/java/Chap1/Main.java
@@ -0,0 +1,24 @@
+package Chap1;
+
+import java.sql.SQLException;
+
+public class Main {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+		UserDao dao = new UserDao();
+		
+		User user = new User();
+		user.setId("whiteship2");
+		user.setName("성진");
+		user.setPassword("testpassword");
+		
+		dao.add(user);
+		
+		System.out.println(user.getId() + " 등록 성공 ");
+		
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		
+		System.out.println(user2.getId() + " 조회 성공 ");
+	}
+}
