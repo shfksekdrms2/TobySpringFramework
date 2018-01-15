e0a1de21b26f3809688018515ee1ae372c59b483
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-01-16 01:37:47 +0900

    싱글톤 레지스트리로 동일 객체가 하나만 생성됨.
    싱글톤 레지스트리가 적용되어 있는지 확인하기위해 User, Connection 객체를 인스턴스 변수로 변경.

--- TobySpringFramework/src/main/java/Chap1/UserDao.java
+++ TobySpringFramework/src/main/java/Chap1/UserDao.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 
 public class UserDao {
 	private ConnectionMaker connectionMaker;
+	private Connection c;
+	private User user;
 	
 	public UserDao(ConnectionMaker connectionMaker){
 		this.connectionMaker = connectionMaker;
@@ -28,7 +30,7 @@ public class UserDao {
 	}
 
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		Connection c = connectionMaker.makeConnection();
+		this.c = connectionMaker.makeConnection();
 
 		PreparedStatement ps = c
 				.prepareStatement("select * from users where id = ?");
@@ -36,15 +38,15 @@ public class UserDao {
 
 		ResultSet rs = ps.executeQuery();
 		rs.next();
-		User user = new User();
-		user.setId(rs.getString("id"));
-		user.setName(rs.getString("name"));
-		user.setPassword(rs.getString("password"));
+		this.user = new User();
+		this.user.setId(rs.getString("id"));
+		this.user.setName(rs.getString("name"));
+		this.user.setPassword(rs.getString("password"));
 
 		rs.close();
 		ps.close();
 		c.close();
 
-		return user;
+		return this.user;
 	}
 }
