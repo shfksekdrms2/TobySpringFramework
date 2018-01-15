61d572c0d51d431a79478fed4320147a332def88
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-01-15 14:37:36 +0900

    DaoFactory 클래스에 스프링을 적용

--- TobySpringFramework/src/main/java/Chap1/DaoFactory.java
+++ TobySpringFramework/src/main/java/Chap1/DaoFactory.java
@@ -1,11 +1,17 @@
 package Chap1;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
-	public UserDao userDao(){
+	@Bean
+	public UserDao userDao() {
 		return new UserDao(connectionMaker());
 	}
-	
-	public ConnectionMaker connectionMaker(){
+
+	@Bean
+	public ConnectionMaker connectionMaker() {
 		return new DConnectionMaker();
 	}
 }
--- TobySpringFramework/src/main/java/Chap1/Main.java
+++ TobySpringFramework/src/main/java/Chap1/Main.java
@@ -2,9 +2,14 @@ package Chap1;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
-		UserDao dao = new DaoFactory().userDao();
+		
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
 		user.setId("whiteship2");
