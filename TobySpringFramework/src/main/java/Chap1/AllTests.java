002bc8a28e50dba3473f0824bdcac32ba7b75b89
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-01-24 02:39:19 +0900

    jUnit를 사용하여 userDao 테스트

---
+++ TobySpringFramework/src/main/java/Chap1/AllTests.java
@@ -0,0 +1,11 @@
+package Chap1;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ UserDaoTest.class })
+public class AllTests {
+
+}
---
+++ TobySpringFramework/src/main/java/Chap1/UserDaoTest.java
@@ -0,0 +1,32 @@
+package Chap1;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+
+public class UserDaoTest {
+	@Test
+	public void addAndGet() throws SQLException, ClassNotFoundException {
+
+		ApplicationContext context = new GenericXmlApplicationContext(
+				"applicationContext.xml");
+		UserDao dao = context.getBean("userDao", UserDao.class);
+
+		User user = new User();
+		user.setId("whiteship2");
+		user.setName("성진");
+		user.setPassword("testpassword");
+
+		dao.add(user);
+
+		User user2 = dao.get(user.getId());
+		
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
+	}
+}
