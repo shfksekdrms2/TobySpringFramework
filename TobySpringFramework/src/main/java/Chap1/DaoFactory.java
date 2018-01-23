187e84f512874d08b6be3c736f2faf3e12beef23
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-01-23 23:34:22 +0900

    db접속방식 변경(dataSource 를 사용)
    compile group: 'org.springframework', name: 'spring-jdbc', version: '3.0.7.RELEASE'
    simpleDriverDataSource를 사용하기 위한 라이브러리 추가

--- TobySpringFramework/src/main/java/Chap1/DaoFactory.java
+++ TobySpringFramework/src/main/java/Chap1/DaoFactory.java
@@ -1,19 +1,35 @@
 package Chap1;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
 public class DaoFactory {
 	@Bean
 	public UserDao userDao() {
 		UserDao userDao = new UserDao();
-		userDao.setConnectionMaker(connectionMaker());
+//		userDao.setConnectionMaker(connectionMaker());
+		userDao.setDataSource(dataSource());
 		return userDao;
 	}
 
 	@Bean
 	public ConnectionMaker connectionMaker() {
 		return new DConnectionMaker();
+	}
+	
+	@Bean
+	public DataSource dataSource(){
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		
+		dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+		dataSource.setUrl("jdbc:mysql://localhost/toby");
+		dataSource.setUsername("root");
+		dataSource.setPassword("manager");
+
+		return dataSource;
 	}
 }
--- TobySpringFramework/src/main/java/Chap1/UserDao.java
+++ TobySpringFramework/src/main/java/Chap1/UserDao.java
@@ -4,18 +4,24 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.sql.DataSource;
 
 public class UserDao {
-	private ConnectionMaker connectionMaker;
+//	private ConnectionMaker connectionMaker;
+	private DataSource dataSource;
 	private Connection c;
 	private User user;
 
-	public void setConnectionMaker(ConnectionMaker connectionMaker) {
-		this.connectionMaker = connectionMaker;
+//	public void setConnectionMaker(ConnectionMaker connectionMaker) {
+//		this.connectionMaker = connectionMaker;
+//	}
+	public void setDataSource(DataSource dataSource){
+		this.dataSource = dataSource;
 	}
 	
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		Connection c = connectionMaker.makeConnection();
+		Connection c = dataSource.getConnection();
+//		Connection c = connectionMaker.makeConnection();
 
 		PreparedStatement ps = c
 				.prepareStatement("insert into users(id, name, password) values(?,?,?)");
@@ -30,7 +36,8 @@ public class UserDao {
 	}
 
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		this.c = connectionMaker.makeConnection();
+		Connection c = dataSource.getConnection();
+//		this.c = connectionMaker.makeConnection();
 
 		PreparedStatement ps = c
 				.prepareStatement("select * from users where id = ?");
