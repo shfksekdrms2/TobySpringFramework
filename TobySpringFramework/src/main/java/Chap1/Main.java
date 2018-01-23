7ca1522e3867821f048211f125292cf222a5c106
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-01-24 01:56:53 +0900

    add와 get에 사용된 값이 같은지 확인하는 기능 추가

--- TobySpringFramework/src/main/java/Chap1/Main.java
+++ TobySpringFramework/src/main/java/Chap1/Main.java
@@ -10,9 +10,12 @@ public class Main {
 	public static void main(String[] args) throws ClassNotFoundException,
 			SQLException {
 
-		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-//		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-//		ApplicationContext context = new AnnotationConfigApplicationContext(CountionDaoFactory.class);
+		ApplicationContext context = new GenericXmlApplicationContext(
+				"applicationContext.xml");
+		// ApplicationContext context = new
+		// AnnotationConfigApplicationContext(DaoFactory.class);
+		// ApplicationContext context = new
+		// AnnotationConfigApplicationContext(CountionDaoFactory.class);
 		UserDao dao = context.getBean("userDao", UserDao.class);
 
 		User user = new User();
@@ -25,12 +28,21 @@ public class Main {
 		System.out.println(user.getId() + " 등록 성공 ");
 
 		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		System.out.println(user2.getPassword());
 
+		if (!user.getName().equals(user2.getName())) {
+			System.out.println("테스트 실패 (name)");
+		} else if (!user.getPassword().equals(user2.getPassword())) {
+			System.out.println("테스트 실패 (password)");
+		} else {
+			System.out.println("조회 테스트 성공");
+		}
+
+		// System.out.println(user2.getName());
+		// System.out.println(user2.getPassword());
 		System.out.println(user2.getId() + " 조회 성공 ");
 
-//		CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
-//		System.out.println("Connection counter: " + ccm.getCounter());
+		// CountingConnectionMaker ccm = context.getBean("connectionMaker",
+		// CountingConnectionMaker.class);
+		// System.out.println("Connection counter: " + ccm.getCounter());
 	}
 }
