44bae35be0889dc351f2f660c06d62bd5fdc3c40
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-01-25 00:49:51 +0900

    deleteAll, getCount 메소드 추가
    deleteAll: DB에 남아 있는 모든 데이터 삭제
    getCount: 남아있는 데이터의 행을 반환

--- TobySpringFramework/src/main/java/Chap1/UserDao.java
+++ TobySpringFramework/src/main/java/Chap1/UserDao.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import javax.sql.DataSource;
+
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 
 public class UserDao {
 //	private ConnectionMaker connectionMaker;
@@ -22,14 +25,12 @@ public class UserDao {
 	public void add(User user) throws ClassNotFoundException, SQLException {
 		Connection c = dataSource.getConnection();
 //		Connection c = connectionMaker.makeConnection();
-
 		PreparedStatement ps = c
 				.prepareStatement("insert into users(id, name, password) values(?,?,?)");
 		ps.setString(1, user.getId());
 		ps.setString(2, user.getName());
 		ps.setString(3, user.getPassword());
-
-		ps.execute();
+			ps.execute();
 
 		ps.close();
 		c.close();
@@ -39,8 +40,7 @@ public class UserDao {
 		Connection c = dataSource.getConnection();
 //		this.c = connectionMaker.makeConnection();
 
-		PreparedStatement ps = c
-				.prepareStatement("select * from users where id = ?");
+		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
 
 		ResultSet rs = ps.executeQuery();
@@ -55,5 +55,31 @@ public class UserDao {
 		c.close();
 
 		return this.user;
+	}
+	
+	public void deleteAll() throws SQLException{
+		Connection c = dataSource.getConnection();
+		
+		PreparedStatement ps = c.prepareStatement("delete from users");
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+	}
+	
+	public int getCount() throws SQLException {
+		Connection c = dataSource.getConnection();
+		
+		PreparedStatement ps = c.prepareStatement("select count(*) from users");
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		int count = rs.getInt(1);
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return count;
 	}
 }
--- TobySpringFramework/src/main/java/Chap1/UserDaoTest.java
+++ TobySpringFramework/src/main/java/Chap1/UserDaoTest.java
@@ -13,9 +13,11 @@ public class UserDaoTest {
 	@Test
 	public void addAndGet() throws SQLException, ClassNotFoundException {
 
-		ApplicationContext context = new GenericXmlApplicationContext(
-				"applicationContext.xml");
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 		UserDao dao = context.getBean("userDao", UserDao.class);
+
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
 
 		User user = new User();
 		user.setId("whiteship2");
@@ -23,9 +25,10 @@ public class UserDaoTest {
 		user.setPassword("testpassword");
 
 		dao.add(user);
+		assertThat(dao.getCount(), is(1));
 
 		User user2 = dao.get(user.getId());
-		
+
 		assertThat(user2.getName(), is(user.getName()));
 		assertThat(user2.getPassword(), is(user.getPassword()));
 	}
