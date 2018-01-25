47f14427be2f31575f8eb091c6caf8d95106b05a
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-01-26 01:27:39 +0900

    Test Case 추가
    - get시 DB에 없는 ID에 대한 예외 케이스 추가
    - User 클래스의 생성자 추가
    - count 메소드 와 getUserFailure 메소드에 대한 테스트 케이스 추가

--- TobySpringFramework/src/main/java/Chap1/UserDao.java
+++ TobySpringFramework/src/main/java/Chap1/UserDao.java
@@ -7,30 +7,31 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+
 import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 
 public class UserDao {
-//	private ConnectionMaker connectionMaker;
+	// private ConnectionMaker connectionMaker;
 	private DataSource dataSource;
 	private Connection c;
 	private User user;
 
-//	public void setConnectionMaker(ConnectionMaker connectionMaker) {
-//		this.connectionMaker = connectionMaker;
-//	}
-	public void setDataSource(DataSource dataSource){
+	// public void setConnectionMaker(ConnectionMaker connectionMaker) {
+	// this.connectionMaker = connectionMaker;
+	// }
+	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
-	
+
 	public void add(User user) throws ClassNotFoundException, SQLException {
 		Connection c = dataSource.getConnection();
-//		Connection c = connectionMaker.makeConnection();
-		PreparedStatement ps = c
-				.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+		// Connection c = connectionMaker.makeConnection();
+		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
 		ps.setString(1, user.getId());
 		ps.setString(2, user.getName());
 		ps.setString(3, user.getPassword());
-			ps.execute();
+		ps.execute();
 
 		ps.close();
 		c.close();
@@ -38,48 +39,53 @@ public class UserDao {
 
 	public User get(String id) throws ClassNotFoundException, SQLException {
 		Connection c = dataSource.getConnection();
-//		this.c = connectionMaker.makeConnection();
+		// this.c = connectionMaker.makeConnection();
 
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
 
 		ResultSet rs = ps.executeQuery();
-		rs.next();
-		this.user = new User();
-		this.user.setId(rs.getString("id"));
-		this.user.setName(rs.getString("name"));
-		this.user.setPassword(rs.getString("password"));
+		this.user = null;
+		if (rs.next()) {
+			this.user = new User();
+			this.user.setId(rs.getString("id"));
+			this.user.setName(rs.getString("name"));
+			this.user.setPassword(rs.getString("password"));
+		}
 
 		rs.close();
 		ps.close();
 		c.close();
+
+		if (this.user == null)
+			throw new EmptyResultDataAccessException(1);
 
 		return this.user;
 	}
-	
-	public void deleteAll() throws SQLException{
+
+	public void deleteAll() throws SQLException {
 		Connection c = dataSource.getConnection();
-		
+
 		PreparedStatement ps = c.prepareStatement("delete from users");
 		ps.executeUpdate();
-		
+
 		ps.close();
 		c.close();
 	}
-	
+
 	public int getCount() throws SQLException {
 		Connection c = dataSource.getConnection();
-		
+
 		PreparedStatement ps = c.prepareStatement("select count(*) from users");
-		
+
 		ResultSet rs = ps.executeQuery();
 		rs.next();
 		int count = rs.getInt(1);
-		
+
 		rs.close();
 		ps.close();
 		c.close();
-		
+
 		return count;
 	}
 }
