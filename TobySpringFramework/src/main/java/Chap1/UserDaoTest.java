37d859839481f88382e867632f8156b4d603fb87
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-02-08 19:15:02 +0900

    before 를 활용한 리팩토링
    공통 픽스처를 before 태그를 이용하여 초기화

--- TobySpringFramework/src/main/java/Chap1/UserDaoTest.java
+++ TobySpringFramework/src/main/java/Chap1/UserDaoTest.java
@@ -2,6 +2,7 @@ package Chap1;
 
 import java.sql.SQLException;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -11,17 +12,26 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
 
 public class UserDaoTest {
+	private UserDao dao;
+	private User user1;
+	private User user2;
+	private User user3;
+
+	@Before
+	public void setUp() {
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		this.dao = context.getBean("userDao", UserDao.class);
+		
+		this.user1 = new User("gyumee", "박성철", "springno1");
+		this.user2 = new User("leegw700", "이길원", "springno2");
+		this.user3 = new User("bumjin", "박범진", "springno3");
+	}
+
 	@Test
 	public void addAndGet() throws SQLException, ClassNotFoundException {
 
-		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-		UserDao dao = context.getBean("userDao", UserDao.class);
-
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
-
-		User user1 = new User("gyumee", "박성철", "springno1");
-		User user2 = new User("leegw700", "이길원", "springno2");
 
 		dao.add(user1);
 		assertThat(dao.getCount(), is(1));
@@ -40,12 +50,6 @@ public class UserDaoTest {
 
 	@Test
 	public void count() throws SQLException, ClassNotFoundException {
-		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-
-		UserDao dao = context.getBean("userDao", UserDao.class);
-		User user1 = new User("gyumee", "박성철", "springno1");
-		User user2 = new User("leegw700", "이길원", "springno2");
-		User user3 = new User("bumjin", "박범진", "springno3");
 
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
@@ -62,9 +66,7 @@ public class UserDaoTest {
 
 	@Test(expected = EmptyResultDataAccessException.class)
 	public void getUserFailure() throws SQLException, ClassNotFoundException {
-		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
-		UserDao dao = context.getBean("userDao", UserDao.class);
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
 
