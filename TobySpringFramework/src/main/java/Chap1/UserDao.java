605a070645bf370ad57a36e8c9b5e476aa71c38d
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-02-27 14:37:24 +0900

    [toby] 유저를 add하는 로직을 수동 DI로 변경

--- TobySpringFramework/src/main/java/Chap1/UserDao.java
+++ TobySpringFramework/src/main/java/Chap1/UserDao.java
@@ -18,35 +18,8 @@ public class UserDao {
 	}
 	
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-
-		try {
-			c = dataSource.getConnection();
-			ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-
-			ps.setString(1, user.getId());
-			ps.setString(2, user.getName());
-			ps.setString(3, user.getPassword());
-			ps.execute();
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-
-				}
-			}
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-
-				}
-			}
-		}
+		StatementStrategy stmt = new AddStatement(user);
+		jdbcContextWithStatementStrategy(stmt);
 	}
 
 	public User get(String id) throws ClassNotFoundException, SQLException {
