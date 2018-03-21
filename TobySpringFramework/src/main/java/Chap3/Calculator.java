2210a39bf57d53fc61c5305c717416ff20d6aff6
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-03-21 21:32:23 +0900

    [toby] 제너릭을 이용한 콜백 인터페이스 변경

--- TobySpringFramework/src/main/java/Chap3/Calculator.java
+++ TobySpringFramework/src/main/java/Chap3/Calculator.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class Calculator {
 	public Integer calcSum(String filepath) throws IOException {
-		LineCallback sumCallback = new LineCallback() {
+		LineCallback<Integer> sumCallback = new LineCallback<Integer>() {
 			@Override
 			public Integer doSomethingWithLine(String line, Integer value) {
 				return value + Integer.valueOf(line);
@@ -16,7 +16,7 @@ public class Calculator {
 	}
 
 	public Integer calcMultiply(String filepath) throws IOException {
-		LineCallback multiplyCallback = new LineCallback() {
+		LineCallback<Integer> multiplyCallback = new LineCallback<Integer>() {
 			@Override
 			public Integer doSomethingWithLine(String line, Integer value) {
 				return value * Integer.valueOf(line);
@@ -25,17 +25,29 @@ public class Calculator {
 		return lineReadTemplate(filepath, multiplyCallback, 1);
 	}
 
-	public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
+	public String concatenate(String filepath) throws IOException {
+		LineCallback<String> concatenateCallback = new LineCallback<String>() {
+			@Override
+			public String doSomethingWithLine(String line, String value) {
+				return value + line;
+			}
+		};
+		return lineReadTemplate(filepath, concatenateCallback, "");
+	}
+
+	public <T> T lineReadTemplate(String filepath, LineCallback<T> callback, T initVal) throws IOException {
 		BufferedReader br = null;
-		Integer res = initVal;
 		try {
 			br = new BufferedReader(new FileReader(filepath));
 			String line = null;
+			T res = initVal;
 			while ((line = br.readLine()) != null) {
 				res = callback.doSomethingWithLine(line, res);
 			}
+			return res;
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
+			throw e;
 		} finally {
 			if (br != null) {
 				try {
@@ -45,6 +57,5 @@ public class Calculator {
 				}
 			}
 		}
-		return res;
 	}
 }
