325ce5d6111760d26334604f5f8cf9247739cae4
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-03-25 18:41:45 +0900

    [toby] 정렬

--- TobySpringFramework/src/main/java/Chap3/CalcSumTest.java
+++ TobySpringFramework/src/main/java/Chap3/CalcSumTest.java
@@ -13,25 +13,25 @@ public class CalcSumTest {
 
 	Calculator calculator;
 	String numFilepath;
-	
+
 	@Before
-	public void setUp(){
+	public void setUp() {
 		this.calculator = new Calculator();
 		this.numFilepath = getClass().getResource("numbers.txt").getPath();
 	}
-	
+
 	@Test
 	public void sumOfNumbers() throws IOException {
 		assertThat(calculator.calcSum(this.numFilepath), is(10));
 	}
-	
+
 	@Test
 	public void multiplyOfNumbers() throws IOException {
 		assertThat(calculator.calcMultiply(this.numFilepath), is(24));
 	}
-	
+
 	@Test
-	public void concatenateStrings() throws IOException{
+	public void concatenateStrings() throws IOException {
 		assertThat(calculator.concatenate(this.numFilepath), is("1234"));
 	}
 }
