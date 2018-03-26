db7633041d3734e6f84176d3c8589925efb14b48
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-03-27 01:59:31 +0900

    [toby] 모든 사용자 정보를 가져오는 getAll 메소드 추가

--- TobySpringFramework/src/main/java/Chap1/UserDao.java
+++ TobySpringFramework/src/main/java/Chap1/UserDao.java
@@ -1,27 +1,19 @@
 package Chap1;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 
 public class UserDao {
-	private DataSource dataSource;
-	private User user;
 	private JdbcTemplate jdbcTemplate;
 
 	public void setDataSource(DataSource dataSource) {
 		jdbcTemplate = new JdbcTemplate(dataSource);
-		this.dataSource = dataSource;
 	}
 
 	public void add(final User user) throws ClassNotFoundException, SQLException {
@@ -46,5 +38,17 @@ public class UserDao {
 
 	public int getCount() throws SQLException {
 		return this.jdbcTemplate.queryForInt("select count(*) from users");
+	}
+
+	public List<User> getAll() {
+		return this.jdbcTemplate.query("select * from users order by id", new RowMapper<User>() {
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				User user = new User();
+				user.setId(rs.getString("id"));
+				user.setName(rs.getString("name"));
+				user.setPassword(rs.getString("password"));
+				return user;
+			}
+		});
 	}
 }
