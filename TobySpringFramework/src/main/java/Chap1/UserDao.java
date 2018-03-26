e4ab26c0a37aae2732ca847721bfe55b3ce3f5d3
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-03-27 02:14:34 +0900

    [toby] rowMapper를 재사용이 가능하도록 변경
    - 중복 제거

--- TobySpringFramework/src/main/java/Chap1/UserDao.java
+++ TobySpringFramework/src/main/java/Chap1/UserDao.java
@@ -16,20 +16,22 @@ public class UserDao {
 		jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
+	private RowMapper<User> userMapper = new RowMapper<User>() {
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			User user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+			return user;
+		}
+	};
+
 	public void add(final User user) throws ClassNotFoundException, SQLException {
 		this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
 	}
 
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[] { id }, new RowMapper<User>() {
-			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-				User user = new User();
-				user.setId(rs.getString("id"));
-				user.setName(rs.getString("name"));
-				user.setPassword(rs.getString("password"));
-				return user;
-			}
-		});
+		return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[] { id }, this.userMapper);
 	}
 
 	public void deleteAll() throws SQLException {
@@ -41,14 +43,6 @@ public class UserDao {
 	}
 
 	public List<User> getAll() {
-		return this.jdbcTemplate.query("select * from users order by id", new RowMapper<User>() {
-			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-				User user = new User();
-				user.setId(rs.getString("id"));
-				user.setName(rs.getString("name"));
-				user.setPassword(rs.getString("password"));
-				return user;
-			}
-		});
+		return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
 	}
 }
