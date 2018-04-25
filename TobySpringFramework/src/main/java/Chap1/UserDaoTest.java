b1857e743d488c0e6ddec7872036a8fa770440b5
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-04-25 17:37:05 +0900

    [toby] throws 예외 삭제
    - spring에서 dataAccessException 으로 throws됨.

--- TobySpringFramework/src/main/java/Chap1/UserDaoTest.java
+++ TobySpringFramework/src/main/java/Chap1/UserDaoTest.java
@@ -33,7 +33,7 @@ public class UserDaoTest {
 	}
 
 	@Test
-	public void addAndGet() throws SQLException, ClassNotFoundException {
+	public void addAndGet() {
 
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
@@ -54,7 +54,7 @@ public class UserDaoTest {
 	}
 
 	@Test
-	public void count() throws SQLException, ClassNotFoundException {
+	public void count() {
 
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
@@ -77,32 +77,32 @@ public class UserDaoTest {
 
 		dao.get("unknown_id");
 	}
-	
+
 	@Test
-	public void getAll() throws ClassNotFoundException, SQLException{
+	public void getAll() {
 		dao.deleteAll();
-		
+
 		List<User> users0 = dao.getAll();
 		assertThat(users0.size(), is(0));
-		
+
 		dao.add(user1);
 		List<User> users1 = dao.getAll();
 		assertThat(users1.size(), is(1));
 		checkSameUser(user1, users1.get(0));
-		
+
 		dao.add(user2);
 		List<User> users2 = dao.getAll();
 		assertThat(users2.size(), is(2));
 		checkSameUser(user1, users2.get(0));
 		checkSameUser(user2, users2.get(1));
-		
+
 		dao.add(user3);
 		List<User> users3 = dao.getAll();
 		assertThat(users3.size(), is(3));
 		checkSameUser(user3, users3.get(0));
 		checkSameUser(user1, users3.get(1));
 		checkSameUser(user2, users3.get(2));
-		
+
 	}
 
 	private void checkSameUser(User user1, User user2) {
