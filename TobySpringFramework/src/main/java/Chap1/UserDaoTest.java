34ffd21af7285fbcbb54569eeb0eb19474544db1
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-05-02 15:57:52 +0900

    [toby] SQLException을 DuplicateKeyException 으로 전환하는 기능 테스트 추가

--- TobySpringFramework/src/main/java/Chap1/UserDaoTest.java
+++ TobySpringFramework/src/main/java/Chap1/UserDaoTest.java
@@ -3,23 +3,30 @@ package Chap1;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.sql.DataSource;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/test-applicationContext.xml")
 public class UserDaoTest {
 	@Autowired
 	private UserDao dao;
+	@Autowired
+	private DataSource dataSource;
 	private User user1;
 	private User user2;
 	private User user3;
@@ -116,5 +123,21 @@ public class UserDaoTest {
 
 		dao.add(user1);
 		dao.add(user1);
+	}
+
+	@Test
+	public void sqlExceptionTranslate() {
+		dao.deleteAll();
+
+		try {
+			dao.add(user1);
+			dao.add(user1);
+		} catch (DuplicateKeyException ex) {
+			SQLException sqEx = (SQLException) ex.getRootCause();
+			SQLExceptionTranslator set = new SQLErrorCodeSQLExceptionTranslator(this.dataSource);
+			// assertThat(set.translate(null, null, sqEx),
+			// is(DuplicateKeyException.class));
+			assertThat(set.translate(null, null, sqEx), is(instanceOf(DuplicateKeyException.class)));
+		}
 	}
 }
