4c0a5d79f58d69d7ab2a191af90cd17d9bfbad15
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-05-03 02:01:26 +0900

    [toby] 사용자 레벨 관리 기능 추가
    4장 서비스 추상화
    - level, login, recommend 인자 추가

--- TobySpringFramework/src/main/java/Chap1/User.java
+++ TobySpringFramework/src/main/java/Chap1/User.java
@@ -4,15 +4,51 @@ public class User {
 	String id;
 	String name;
 	String password;
-	
-	public User(String id, String name, String password){
+	Level level;
+	int login;
+	int recommend;
+
+	public User() {
+
+	}
+
+	public User(String id, String name, String password) {
 		this.id = id;
 		this.name = name;
 		this.password = password;
 	}
-	
-	public User() {
 
+	public User(String id, String name, String password, Level level, int login, int recommend) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.level = level;
+		this.login = login;
+		this.recommend = recommend;
+	}
+
+	public Level getLevel() {
+		return level;
+	}
+
+	public void setLevel(Level level) {
+		this.level = level;
+	}
+
+	public int getLogin() {
+		return login;
+	}
+
+	public void setLogin(int login) {
+		this.login = login;
+	}
+
+	public int getRecommend() {
+		return recommend;
+	}
+
+	public void setRecommend(int recommend) {
+		this.recommend = recommend;
 	}
 
 	public String getId() {
@@ -39,4 +75,29 @@ public class User {
 		this.password = password;
 	}
 
+	public enum Level {
+		BASIC(1), SILVER(2), GOLD(3);
+		private final int value;
+
+		Level(int value) {
+			this.value = value;
+		}
+
+		public int intValue() {
+			return value;
+		}
+
+		public static Level valueOf(int value) {
+			switch (value) {
+				case 1:
+					return BASIC;
+				case 2:
+					return SILVER;
+				case 3:
+					return GOLD;
+				default:
+					throw new AssertionError("Unknown value: " + value);
+			}
+		}
+	}
 }
--- TobySpringFramework/src/main/java/Chap1/UserDaoTest.java
+++ TobySpringFramework/src/main/java/Chap1/UserDaoTest.java
@@ -17,6 +17,7 @@ import org.springframework.jdbc.support.SQLExceptionTranslator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import Chap1.User.Level;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
 
@@ -33,9 +34,9 @@ public class UserDaoTest {
 
 	@Before
 	public void setUp() {
-		this.user1 = new User("gyumee", "박성철", "springno1");
-		this.user2 = new User("leegw700", "이길원", "springno2");
-		this.user3 = new User("bumjin", "박범진", "springno3");
+		this.user1 = new User("gyumee", "박성철", "springno1", Level.BASIC, 1, 0);
+		this.user2 = new User("leegw700", "이길원", "springno2", Level.SILVER, 55, 10);
+		this.user3 = new User("bumjin", "박범진", "springno3", Level.GOLD, 100, 40);
 	}
 
 	@Test
@@ -51,12 +52,10 @@ public class UserDaoTest {
 		assertThat(dao.getCount(), is(2));
 
 		User userget1 = dao.get(user1.getId());
-		assertThat(userget1.getName(), is(user1.getName()));
-		assertThat(userget1.getPassword(), is(user1.getPassword()));
+		checkSameUser(userget1, user1);
 
 		User userget2 = dao.get(user2.getId());
-		assertThat(userget2.getName(), is(user2.getName()));
-		assertThat(userget2.getPassword(), is(user2.getPassword()));
+		checkSameUser(userget2, user2);
 	}
 
 	@Test
@@ -115,6 +114,9 @@ public class UserDaoTest {
 		assertThat(user1.getId(), is(user2.getId()));
 		assertThat(user1.getName(), is(user2.getName()));
 		assertThat(user1.getPassword(), is(user2.getPassword()));
+		assertThat(user1.getLevel(), is(user2.getLevel()));
+		assertThat(user1.getLogin(), is(user2.getLogin()));
+		assertThat(user1.getRecommend(), is(user2.getRecommend()));
 	}
 
 	@Test(expected = DataAccessException.class)
