6fe1e8fe562fcb48fdd084c523558aab3495bada
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-05-10 01:09:20 +0900

    [toby] 사용자 정보 수정 기능 추가

--- TobySpringFramework/src/main/java/Chap1/UserDaoTest.java
+++ TobySpringFramework/src/main/java/Chap1/UserDaoTest.java
@@ -149,6 +149,7 @@ public class UserDaoTest {
 		dao.deleteAll();
 		
 		dao.add(user1);
+		dao.add(user2);
 		
 		user1.setName("오민규");
 		user1.setPassword("springno6");
@@ -159,5 +160,7 @@ public class UserDaoTest {
 		
 		User user1update = dao.get(user1.getId());
 		checkSameUser(user1, user1update);
+		User user2same = dao.get(user2.getId());
+		checkSameUser(user2, user2same);
 	}
 }
