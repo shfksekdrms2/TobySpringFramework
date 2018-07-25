eb3a7ad56e8ea5703254e722ea9ae78a95111cf8
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-07-25 14:05:38 +0900

    [toby] java mail 발송 기능 추가 - 메일 서버가 없어 Error 발생

--- TobySpringFramework/src/main/java/Chap1/UserService.java
+++ TobySpringFramework/src/main/java/Chap1/UserService.java
@@ -2,10 +2,16 @@ package Chap1;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 
-import javax.sql.DataSource;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
@@ -43,6 +49,26 @@ public class UserService {
 	protected void upgradeLevel(User user) {
 		user.upgradeLevel();
 		userDao.update(user);
+		sendUpgradeEmail(user);
+	}
+
+	private void sendUpgradeEmail(User user) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "mail.ksug.org");
+		Session s = Session.getInstance(props, null);
+
+		MimeMessage message = new MimeMessage(s);
+		try {
+			message.setFrom(new InternetAddress("useradmin@ksug.org"));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+			message.setSubject("Upgrade 안내");
+			message.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드되었습니다.");
+			Transport.send(message);
+		} catch (AddressException e) {
+			throw new RuntimeException(e);
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
