a257fa4b5aab64ce193b5499dd9c4e11a8de2fff
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-08-09 21:57:40 +0900

    [toby] 정렬

--- TobySpringFramework/src/main/java/Chap1/MailSender.java
+++ TobySpringFramework/src/main/java/Chap1/MailSender.java
@@ -3,8 +3,8 @@ package Chap1;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 
-
 public interface MailSender {
 	void send(SimpleMailMessage simpleMessage) throws MailException;
+
 	void send(SimpleMailMessage[] simpleMessage) throws MailException;
 }
